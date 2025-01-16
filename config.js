c2941175b9997c20e3beb9b61a8d9ac07d558323
oshadaX-Wolf <153474645+oshadaX-Wolf@users.noreply.github.com>
2025-01-17 00:22:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
    SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=p691CSrb#SGMgpqj7_ViW6JLFgz0EydcyZ-LhxHNrlh4A-vcx5mE' : process.env.SESSION_ID, 
-   DATABASE_URL: process.env.DATABASE_URL === undefined ? 'postgres://movie_my_user:BDXztL7cmv1gV26b9eCsAseSMp7tqyvW@dpg-co1n7jvsc6pc73ctrku0-a.oregon-postgres.render.com/movie_my' : process.env.DATABASE_URL,
+   DATABASE_URL: process.env.DATABASE_URL === undefined ? 'postgres://koyeb-adm:qFWVex1dO6Mt@ep-shrill-surf-a22zn6z2.eu-central-1.pg.koyeb.app/koyebdb' : process.env.DATABASE_URL,
    PREFIX: process.env.PREFIX || '.' ,
    JID: process.env.JID || `94755773910@s.whatsapp.net` , 
    FOOTER: process.env.FOOTER === undefined ? 'TC TEAM MOVIE DL' : process.env.FOOTER
